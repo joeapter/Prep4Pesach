12672e43bd13e38cf10100b5c093e22f3bc9c13d
Joe Apter <joeapter@gmail.com>
2026-01-28 22:27:30 +0200

    Fix Next build with PWA + webpack

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true
-  }
+  },
+  turbopack: {}
 };
 
 module.exports = withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    skipWaiting: true
-  }
+  ...nextConfig
 });
